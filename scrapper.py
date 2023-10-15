b5935166ca4982ea042f80e5bfc751c87a9f6557
Matias Rodriguez <matiasrodriguez@MR-Macbook-Air.fibertel.com.ar>
2023-10-15 10:51:34 -0300

    changes in scrapper

--- scrapper.py
+++ scrapper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import gspread
+import time
 
 gc = gspread.service_account('credentials.json')
 
@@ -12,16 +13,18 @@ gc = gspread.service_account('credentials.json')
 sheet = gc.open("Remates").sheet1
 
 def extract_price_from_url(URL):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
 
-    browser = webdriver.Chrome(options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+
+    # Inicializa el WebDriver sin especificar la ruta del ejecutable
+    browser = webdriver.Chrome(options=chrome_options)
+
     browser.get(URL)
 
     try:
         precio_div = WebDriverWait(browser, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "offer-bid-panel"))
-        )
+            EC.presence_of_element_located((By.CLASS_NAME, "offer-bid-panel")))
         
         soup = BeautifulSoup(browser.page_source, 'html.parser')
         precio_div_bs4 = soup.find("span", {"class": "lance-atual"})
@@ -43,11 +46,12 @@ def convert_to_float(number_str):
     clean_number = number_str.replace('.', '').replace(',', '.')
     return float(clean_number)
 
-for row_number in range(2, 51):
-    link = sheet.cell(row_number, 7).value
-    print(f"Procesando URL en fila {row_number}: {link}")  # Agregar esta línea
+for row_number in range(2, 127):
+    link = sheet.cell(row_number, 9).value
+    patente = sheet.cell(row_number, 7).value
+    print(f"Procesando URL en fila {row_number}: {patente}")  # Agregar esta línea
     if link:
         price = extract_price_from_url(link)
         if price:
             float_price = convert_to_float(price)
-            sheet.update_cell(row_number, 8, float_price)
+            sheet.update_cell(row_number, 10, float_price)
